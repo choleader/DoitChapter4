28a2f3848a1fc54be4448098ed581e8e4b07ed00
Choleader <sangyeon107@gmail.com>
2022-08-31 15:44:42 +0900

    doit chapter4

---
+++ src/ifExample/IfExample1.java
@@ -0,0 +1,18 @@
+package ifExample;
+
+public class IfExample1 {
+
+	public static void main(String[] args) {
+		
+		int age = 7;
+		
+		if(age >= 8) {
+			System.out.println("학교 다닙니다.");
+		}
+		else {
+			System.out.println("학교를 다니지 않습니다.");
+		}
+
+	}
+
+}
---
+++ src/ifExample/SwitchCase1.java
@@ -0,0 +1,22 @@
+package ifExample;
+
+public class SwitchCase1 {
+
+	public static void main(String[] args) {
+		
+		int rank = 6;
+		char medalColor;
+		switch(rank) {
+			case 1 : medalColor = 'G';
+				break;
+			case 2 : medalColor = 'S';
+				break;
+			case 3 : medalColor = 'B';
+				break;
+			default: medalColor = 'A';
+		}
+		System.out.println(rank + "등 메달의 색깔은" + medalColor + "입니다.");
+
+	}
+
+}
